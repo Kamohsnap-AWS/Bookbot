3eff4a39794fedf90cf8514ced4e83a402c2416e
Kamohsnap_AWS <kamohsnapaws@gmail.com>
2025-01-29 16:57:04 -0500

    updated  main() and completed print_report()

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def main():
-    # Used to open book and stores the contents
-    with open("Books/frankenstein.txt") as f:
-        file_contents = f.read()
+    file_path = "Books/frankenstein.txt"
+    book_contents = get_book_text(file_path)
+    num_words = get_word_count(book_contents)
+    char_dict = get_char_count(book_contents)
 
-    print(get_word_count(file_contents))
-    
-    print(get_char_count(file_contents))
+    print_report(num_words, char_dict)
 
-# Counts the number of words in our current book
+def get_book_text(path):
+    # Opens book and returns the contents
+    with open(path) as f:
+        return f.read()
+
 def get_word_count(text):
+    # Returns the number of words in our current book
     return len(text.split())    
 
 def get_char_count(text):
@@ -25,6 +29,15 @@ def get_char_count(text):
 
     return chars
 
+def print_report(w_count, c_dict):
+    print("--- Begin Report ---")
+    print(f"{w_count} words were found in the document\n")
 
+    # Prints all the alphabetical characters and the number of times they appear
+    for key in c_dict:
+        if key.isalpha():
+            print(f"The '{key}' character was found {c_dict[key]} times")
+
+    print("\n--- End Report ---")
 
 main()
