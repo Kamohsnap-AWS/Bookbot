3e14964ae0c8cf738d5e3d3ed4996adb02a63790
Kamohsnap_AWS <kamohsnapaws@gmail.com>
2025-03-09 18:38:55 -0400

    updated main.py: added input validation preventing books not in our library from being given.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import sys, os
 from stats import *
 
-import sys
-
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-
+    verify_arg_count()
     file_path = sys.argv[1]
+    verify_valid_args(file_path)
+
     book_contents = get_book_text(file_path)
     word_count = get_word_count(book_contents)
     character_dict = get_char_count(book_contents)
     print_report(word_count, character_dict)
+
+def verify_arg_count():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+def verify_valid_args(path):
+    if not os.path.exists(path):
+        print("The given <path_to_book> does not exist in our library. Enter a valid <path_to_book>.")
+        sys.exit(1)
 
 def get_book_text(path):
     # Opens book and returns the contents
